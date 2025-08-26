16fb5ff599e35e76dfb2e938c16a2fe83c834c2e
ADL13 <a.d.l.13.02.73@gmail.com>
2025-08-26 19:42:00 +0500

    Домашнее задание к занятию- доделана задача 3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,36 +5,47 @@ let deadCount = 0;
 let lostCount = 0;
 const totalHoles = 9;
 let gameOver = false;
+
 function getHole(index) {
-    return document.getElementById(`hole${index}`);
-  }
-
-for (let i = 1; i <= totalHoles; i++) {
-  const hole = getHole(i);
-  hole.addEventListener("click", () => {
-    if (gameOver) return;
-
-    if (hole.classList.contains("hole_has-mole")) {
-      deadCount++;
-      deadSpan.textContent = deadCount;
-
-      hole.classList.remove("hole_has-mole");
-      
-      if (deadCount >= 10) {
-        alert("Победа!");
-        endGame();
-      }
-    } else {
-      lostCount++;
-      lostSpan.textContent = lostCount;
-      if (lostCount >= 5) {
-        alert("Игра окончена!");
-        endGame();
-      }
-    }
-  });
+  return document.getElementById(`hole${index}`);
 }
 
+function handleClick(e) {
+  if (gameOver) return;
+  const hole = e.currentTarget;
+  if (hole.classList.contains("hole_has-mole")) {
+    deadCount++;
+    deadSpan.textContent = deadCount;
+
+    hole.classList.remove("hole_has-mole");
+
+    if (deadCount >= 10) {
+      alert("Победа!");
+      endGame();
+    }
+  } else {
+    lostCount++;
+    lostSpan.textContent = lostCount;
+    if (lostCount >= 5) {
+      alert("Игра окончена!");
+      endGame();
+    }
+  }
+}
+
+for (let i = 1; i <= totalHoles; i++) {
+  getHole(i).addEventListener("click", handleClick);
+}
 function endGame() {
-    gameOver = true;
+  gameOver = true;
+  setTimeout(resetGame, 2000);
+}
+
+function resetGame() {
+  deadCount = 0;
+  lostCount = 0;
+  deadSpan.textContent = deadCount;
+  lostSpan.textContent = lostCount;
+
+  gameOver = false;
 }
