9bdb34e40e96696c012d85c0837e323df12e4740
ADL13 <a.d.l.13.02.73@gmail.com>
2025-08-30 14:34:28 +0500

    Домашнее задание к лекции «DOM»

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,19 +1,25 @@
-const fontSizeElements = document.querySelectorAll('.font-size');
-const book = document.querySelector('.book');
+const fontSizeElements = document.querySelectorAll(".font-size");
+const book = document.querySelector(".book");
 
-fontSizeElements.forEach(element => {
-    element.addEventListener('click', function (e) {
-        e.preventDefault();
+fontSizeElements.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        fontSizeElements.forEach(el => el.classList.remove('font-size_active'));
-        this.classList.add('font-size_active');
-        book.classList.remove('font-size_small', 'font-size_big'); 
+    const isActive = this.classList.contains("font-sixe_active");
 
-        const size = this.dataset.size;
-        if (size === 'small') {
-            book.classList.add('font-size_small');
-        } else {
-            book.classList.add('font-size_big');
-        }
-    });
+    fontSizeElements.forEach((el) => el.classList.remove("font-size_active"));
+    this.classList.add("font-size_active");
+    book.classList.remove("font-size_small", "font-size_big");
+
+    if (!isActive) {
+      this.classList.add("font-size_active");
+
+      const size = this.dataset.size;
+      if (size === "small") {
+        book.classList.add("font-size_small");
+      } else if (size === "big") {
+        book.classList.add("font-size_big");
+      }
+    }
+  });
 });
